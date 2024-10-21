9f3406199018d8fcc48da14c3884237c231e2dcf
ralstonnn <zigthehedge2002@gmail.com>
2024-10-21 23:53:20 +0500

    starting to implement encryptioin

---
+++ include/encryption.h
@@ -0,0 +1,7 @@
+#ifndef ENCTYPRION_H
+#define ENCTYPRION_H
+
+void encrypt_str(char *str);
+void dencrypt_str(char *str);
+
+#endif
---
+++ src/encryption.c
@@ -0,0 +1,7 @@
+void encrypt_str(char *str) {
+    
+}
+
+void dencrypt_str(char *str) {
+
+}
