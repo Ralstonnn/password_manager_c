9b5a2ab3316f95e450a5a4bc4a592d27602955dc
ralstonnn <zigthehedge2002@gmail.com>
2024-10-30 00:23:57 +0500

    db migrations

--- include/db.h
+++ include/db.h
@@ -5,6 +5,7 @@
 
 #define DB_FILE_PATH "../test.db"
 
+int init_db();
 int request_db(char *sql, void *data,
                int(callback)(void *data, int argc, char **argv,
                              char **azColName));
@@ -13,4 +14,11 @@ int search_records_db(struct account_record *records, char *search_str,
 int save_record_db(struct account_record record);
 int get_record_by_id_db(struct account_record *record, int id);
 
+#define DB_MIGRATION                                                           \
+    "CREATE TABLE records ("                                                   \
+    "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"                           \
+    "name TEXT NOT NULL,"                                                      \
+    "username TEXT NOT NULL,"                                                  \
+    "password TEXT NOT NULL"                                                   \
+    ");"
 #endif
--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "db.h"
 
 struct account_record split(char *str);
 
@@ -11,6 +12,10 @@ int main() {
     // Called for rand() to work
     srand(time(0));
 
+    if (!init_db()) {
+        printf("Failed to initialize database\n");
+        return 1;
+    }
     if (!encryption_init()) {
         printf("Failed to initialize encryption\n");
         return 1;
